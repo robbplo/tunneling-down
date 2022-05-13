6699b819db96b7b9e6390e4bf3b6297762a2fdb1
Robbin Ploeger <robbplo@gmail.com>
2022-05-13 14:32:42 +0200

    Getting resourceful

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CarResource;
 use App\Models\Car;
 
 class CarController extends Controller
 {
     public function show(Car $car)
     {
-        return $car;
+        return new CarResource($car);
     }
 }
---
+++ app/Http/Resources/CarResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CarResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'brand_id' => $this->brand_id,
+            'model_name' => $this->model_name,
+            'manufacturing_year' => $this->manufacturing_year,
+            'price' => $this->price,
+            'mileage' => $this->mileage,
+        ];
+    }
+}
